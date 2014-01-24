c44eada2aa303a24d79a8b7554bd0f94be7bedbc
Mikael Brassman <mishakun@gmail.com>
2014-01-24 17:09:59 +0100

    Updated logger

--- www/scripts/game/log.js
+++ www/scripts/game/log.js
@@ -1,12 +1,22 @@
 define([], function() {
    
    var exports = {},
-       debug = false || (document.location.hostname == "localhost");
+       debug = false || (document.location.hostname == "localhost"),
+       category = 'math-trainer';
 
    exports.event = function(action, label) {
-       ga('send', 'event', 'math-trainer', action, label);
+       ga('send', 'event', category, action, label);
        if (debug) {
-           console.log('send', 'event', 'math-trainer', action, label);
+           console.log('send', 'event', category, action, label);
+       }
+   };
+
+   exports.timing = function(action, duration, label) {
+       if (duration > 0) {
+           ga('send', 'timing', category, action, duration, label);
+           if (debug) {
+               console.log('send', 'timing', category, action, duration, label);
+           }
        }
    };
 
